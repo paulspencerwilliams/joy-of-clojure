d6dabf992dc67654ad08f76011cf20ca005f40e4
Paul Williams <paul@paulswilliams.me.uk>
2015-03-29 11:56:12 +0100

    Initial project creation

---
+++ README.md
@@ -0,0 +1,10 @@
+# joy-of-clojure
+
+This is a scratchpad for another run through of The Joy of Clojure, 2nd edition by Micheal Fogus and Chris Houser. 
+
+## License
+
+Copyright © 2015 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject joy-of-clojure "0.1.0-SNAPSHOT"
+  :description "Another run through the Joy of Clojure second edition"
+  :url "http://github.com/paulspencerwilliams/joy-of-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
