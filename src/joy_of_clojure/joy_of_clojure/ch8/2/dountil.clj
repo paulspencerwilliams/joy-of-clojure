cb24965864e5511df62049425a491166aecdc684
Paul Williams <paul@paulswilliams.me.uk>
2015-06-29 19:23:48 +0100

    Chapter 8.2 - Macros!

---
+++ src/joy_of_clojure/joy_of_clojure/ch8/2/dountil.clj
@@ -0,0 +1,20 @@
+(ns joy-of-clojure.joy-of-clojure.ch8.2.dountil)
+
+(defmacro do-until [& clauses]
+  (when clauses
+    (list 'clojure.core/when (first clauses)
+          (if (next clauses)
+            (second clauses)
+            (throw (IllegalArgumentException.
+                     "do-until requires an even number of forms")))
+          (cons 'do-until (nnext clauses))))
+  )
+
+(macroexpand-1 '(do-until true (prn 1) false (prn 2)))
+
+(require '[clojure.walk :as walk])
+
+(walk/macroexpand-all '(do-until true (prn 1) false (prn 2)))
+
+
+(do-until true (prn 1) false (prn 2))
